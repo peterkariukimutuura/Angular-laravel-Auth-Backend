760844013610979717fb6072ee45e8b661a3536c
peterkariukimutuura <kariukipeter30@gmail.com>
2018-10-27 08:54:21 +0300

    backend tuning

--- app/User.php
+++ app/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable implements JWTSubject
         {
             return [];
         }
+
+        public function setPasswordAttribute($value){
+            $this->attributes['password']= bcrypt($value);
+        }
 }
